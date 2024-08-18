ed10f74c6073f1092b5e0de850d875b9af8fe3ca
Om Shandilya <omshandilyacoading@gmail.com>
2024-08-18 18:02:36 +0530

    second commit

--- src/routes/tweet.routes.ts
+++ src/routes/tweet.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { getTweetController,getAllTweetsController, createTweetController, updateTweetController, deleteTweetController } from "../controllers/tweet.controller";
 const tweetRouter = Router()
 tweetRouter.get("/:tweetId",getTweetController)
-tweetRouter.get("/",getAllTweetsController)
+tweetRouter.get("/get/all",getAllTweetsController)
 tweetRouter.post("/", createTweetController)
 tweetRouter.delete("/:tweetId", deleteTweetController)
 tweetRouter.put("/", updateTweetController)
